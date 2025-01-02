e2f08218ce975eb463f4caf3a1315a0e19d33706
Daniel Hodgins <121420574+DanielHodgins@users.noreply.github.com>
2025-01-02 00:13:23 -0600

    Add files via upload

---
+++ 2015/aoc_2015_num12.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  2 00:05:50 2025
+
+@author: danie
+"""
+
+import numpy as np
+
+
+apple = np.zeros((1000, 1000))
+
+
+bag=input()
+
+for lizards in range(len(bag.splitlines())):
+    command = bag.splitlines()[lizards]
+    parts = command.split()
+    if parts[0]=='toggle':
+        start_coords = list(map(int, parts[1].split(',')))
+        end_coords = list(map(int, parts[3].split(',')))
+
+        # Perform the action on the grid
+        for i in range(start_coords[0], end_coords[0] + 1):
+            for j in range(start_coords[1], end_coords[1] + 1):
+                apple[i][j] +=2
+    if parts[0]=='turn':
+        start_coords = list(map(int, parts[2].split(',')))
+        end_coords = list(map(int, parts[4].split(',')))
+
+        # Perform the action on the grid
+        for i in range(start_coords[0], end_coords[0] + 1):
+            for j in range(start_coords[1], end_coords[1] + 1):
+                if parts[1] == "on":
+                    apple[i][j] += 1
+                if parts[1] == "off":
+                    if apple[i][j]>0:
+                        apple[i][j]+= -1
+lights=0
+for i in range(1000):
+    for j in range(1000):
+        lights+=apple[i][j]
+print('There are', lights, 'lights on')
